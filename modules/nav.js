2968f619fb146f5319db4dc7e54bc8e5b4c2c4e5
Precious <lukeprecious@gmail.com>
2020-10-07 16:31:16 +0100

    first

---
+++ modules/nav.js
@@ -0,0 +1,28 @@
+const Header =()=> 
+
+`
+<div class="nav-container">
+			<div class="logo-ref">
+				<img src="/BugsBunny.png">
+			</div>
+			<div class="listed-items">
+				<ul class="some-cases">
+					<li>
+						<a href="contact">Home</a>
+					</li>
+					<li>
+						<a href="about.html">About</a>
+					</li>
+					
+				</ul>
+
+				<div id="sign-in">
+					<button onclick="myFunction()">Sign In</button>
+				</div>
+
+			</div>
+
+		</div>
+`
+
+export default Header;
